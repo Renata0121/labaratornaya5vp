df759405f63921cc9d06230d6e7547e7f9ba458b
Renata0121 <54933531+Renata0121@users.noreply.github.com>
2019-09-22 16:12:24 +0300

    Add files via upload

---
+++ лаба5задание1вп.cpp
@@ -0,0 +1,17 @@
+﻿// лаба5задание1вп.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int k, b;
+	cout << "Введите b";
+	cin >> b;
+	k = b / 1024;
+	cout << "k=" << k ;
+	return 0;
+}
